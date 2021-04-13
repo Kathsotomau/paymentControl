ce11fe58ba3d689ac332952d367a317f13c4eb8b
Kathsotomau <kat.soto.maureira@gmail.com>
2021-04-13 00:10:03 -0400

    beginning proyect my finance control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentListComponent } from './payment-list/payment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentListComponent
   ],
   imports: [
     BrowserModule,
